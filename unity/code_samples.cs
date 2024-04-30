17114371cb70e70e59fd348211008a04a8811635
lgr1s <132448934+lgr1s@users.noreply.github.com>
2024-04-30 11:33:17 +0200

    Update code_samples.cs

--- unity/code_samples.cs
+++ unity/code_samples.cs
@@ -6,3 +6,21 @@
         }
     }
 }
+// Coin rotation
+
+{
+
+    private float speed = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed, Space.World);
+    }
+}
